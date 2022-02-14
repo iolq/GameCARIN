2bcbe1659236abbee0d93762da664702064b4b44
Qaizx <kaothom@gmail.com>
2022-02-14 22:00:38 +0700

    เพิ่มโครงสร้างของ parser ทั้งหมด

---
+++ code-back/src/GeneticCode.java
@@ -0,0 +1,67 @@
+import java.beans.Expression;
+
+public class GeneticCode {
+    Expression Program(){
+        return  null;
+    }
+
+    Expression Statement(){
+        return null;
+    }
+
+    Expression Command(){
+        return null;
+    }
+
+    Expression AssignmentStatement(){
+        return null;
+    }
+
+    Expression ActionCommand(){
+        return null;
+    }
+
+    Expression MoveCommand(){
+        return null;
+    }
+
+    Expression AttackCommand(){
+        return null;
+    }
+
+    Expression Direction(){
+        return null;
+    }
+
+    Expression BlockStatement(){
+        return null;
+    }
+
+    Expression IfStatement(){
+        return null;
+    }
+
+    Expression WhileStatement(){
+        return null;
+    }
+
+    Expression Expression(){
+        return null;
+    }
+
+    Expression Term(){
+        return null;
+    }
+
+    Expression Factor(){
+        return null;
+    }
+
+    Expression Power(){
+        return null;
+    }
+
+    Expression SensorExpression(){
+        return null;
+    }
+}
