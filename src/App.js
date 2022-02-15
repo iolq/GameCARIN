a6c62a89ebfce38a1685623f1bc70ff3620f581d
iolq <wipawee_watt@cmu.ac.th>
2022-02-15 17:47:25 +0700

    updated5.42pm

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src='/lobby.jpg'></img>
+      </header>
+    </div>
+  );
+}
+
+export default App;
