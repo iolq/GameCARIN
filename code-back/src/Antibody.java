6e9791d88334f3016097df2cf25c348726471245
Qaizx <kaothom@gmail.com>
2022-02-15 16:12:55 +0700

    สร้าง class virus กับ antibody ขึ้นมา

---
+++ code-back/src/Antibody.java
@@ -0,0 +1,31 @@
+public class Antibody implements Cell{
+    @Override
+    public int moveTo() {
+        return 0;
+    }
+
+    @Override
+    public int shootTo() {
+        return 0;
+    }
+
+    @Override
+    public int Armor() {
+        return 0;
+    }
+
+    @Override
+    public int Hp() {
+        return 0;
+    }
+
+    @Override
+    public int DeHp() {
+        return 0;
+    }
+
+    @Override
+    public int DeArmor() {
+        return 0;
+    }
+}
