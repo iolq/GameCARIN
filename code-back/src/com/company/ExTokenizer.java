76f085ac8317a243239cb744cce2759a7256fa22
Qaizx <kaothom@gmail.com>
2022-02-16 22:25:32 +0700

    เปลี่ยน linklist<Pair> เป็น Pair และ เพิ่ม func computeNext การ return ค่าแต่ละค่าของ Pair

--- code-back/src/com/company/ExTokenizer.java
+++ code-back/src/com/company/ExTokenizer.java
@@ -4,24 +4,81 @@ import java.util.Iterator;
 import java.util.LinkedList;
 
 public class ExTokenizer implements Tokenizer{
-    LinkedList<Pair<String,String>> list;
-    Iterator<Pair<String, String>> run;
+    Pair<String,String> list;
+    private String src;
+    private String next;
+    private int position;
 
-
-    ExTokenizer(LinkedList<Pair<String,String>> ac){
+    ExTokenizer(Pair<String,String> ac){
         list = ac;
-        run = list.iterator();
     }
+
+    private void computeNext() {
+        StringBuilder s = new StringBuilder();
+        //ตัดช่องว่าง
+        while(position<src.length() && isSpace(src.charAt(position))) position++;
+
+        if(position<src.length()){
+            char c = src.charAt(position);
+            if(Character.isDigit(c)){
+                s.append(c);
+                for(position++;position<src.length() && Character.isDigit(src.charAt(position));position++){
+                    s.append(src.charAt(position));
+                }
+            }else if(isCharacter(c)){
+                System.out.println("SyntaxError");
+            }else if(c=='+'||c=='-'||c=='('||c==')'||c=='*'||c=='/'||c=='%'){
+                s.append(c);position++;
+            }else{
+                System.out.println("SyntaxError");
+            }
+        }
+        next = s.toString();
+    }
+
+    private boolean isSpace(char c){
+        if(c == ' '){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isCharacter(char c){
+        if((int) c <= 65 && (int) c >= 122){
+            return true;
+        }
+        return false;
+    }
+
 
     @Override
     public String peek() {
-        Iterator<Pair<String, String>> peek = run;
-        Pair<String, String> re = peek.next();
-        return re.fst;
+
+        return list.fst;
     }
 
     @Override
-    public Pair<String, String> consume() {
-        return run.next();
+    public String consume() {
+        String result = next;
+            computeNext();
+        return result;
+    }
+
+    @Override
+    public String command() {
+        return list.snd;
+    }
+
+    boolean peek(String s) {
+        if(s == null){
+            return false;
+        }
+        return peek().equals(s);
+    }
+
+    void consume(String s) {
+        if(peek(s)){
+            consume();
+        }
     }
 }
