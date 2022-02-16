8ce68ef70638e17afb43281992d9fd0c400b7d16
iolq <wipawee_watt@cmu.ac.th>
2022-02-16 16:09:31 +0700

    updated lobby  16/02 4.09pm

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div class="container">
+        <div className='homepage-bgimage '>
+          <div class="btn-holder">
+            <button className='button-corner '>START</button>
+          </div>
+        </div>
 
-      <div className='homepage-bgimage '>
-        <div style={{ display: "flex" }}>
-          <button
-            className='button'
-            style={{ marginLeft: "1000px" }}>START</button></div></div>
-
+      </div >
     </div>
   );
 }
