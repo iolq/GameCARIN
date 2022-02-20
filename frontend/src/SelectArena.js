d0cb8cf8e9fd1b8de2f2999458af0c4b33aa2f32
iolq <wipawee_watt@cmu.ac.th>
2022-02-20 19:09:04 +0700

    updated SelectArena's background added 7.09pm 20/2

--- frontend/src/SelectArena.js
+++ frontend/src/SelectArena.js
@@ -26,7 +26,7 @@ const BannerBtn = styled.a`
 }
 `;
 const Button_corner = styled.a` 
-    background-color: white;
+    /* background-color: white; */
     color: black;
     
     border-radius: 24px;
@@ -53,22 +53,31 @@ const Row = styled.div`
   }
 `
 const Container = styled.div`
-    max-width: 960px;
+    max-width: 1000px;
     margin: 0 auto;
     justify-content: center;
 `
+const Bgimage = styled.div`
+    background: url("https://scontent.fcnx4-1.fna.fbcdn.net/v/t1.15752-9/274160774_391891562700164_1152465655997423887_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=ae9488&_nc_eui2=AeH5_x4T-CB8RRuh2_CuJvLYJDhWFfO7WSwkOFYV87tZLLUkAah4Iv4NoDNhUxPq8BPjt7yMI83ktBnRdlTZvWh-&_nc_ohc=A0uRMaar5IIAX9hKH7C&_nc_ht=scontent.fcnx4-1.fna&oh=03_AVIlxphd3U3n9jKIMM3y3dbAWCU1E2ACnbdElu_Hkx8aGg&oe=62394630");
+    height: 100vh;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: contain ;
+  
+    `;
 function SelectArena() {
     return (
 
         <Container>
-            <BannerBtn href="/Dressing">back</BannerBtn>
+            <Bgimage>
+                <BannerBtn href="/Dressing">back</BannerBtn>
 
-            <Button_corner>
-                <BannerBtn href="/SelectArena">EASY</BannerBtn>
-                <BannerBtn href="/SelectArena">MEDIUM</BannerBtn>
-                <BannerBtn href="/SelectArena">HARD</BannerBtn>
-            </Button_corner>
-
+                <Button_corner>
+                    <BannerBtn href="/SelectArena">EASY</BannerBtn>
+                    <BannerBtn href="/SelectArena">MEDIUM</BannerBtn>
+                    <BannerBtn href="/SelectArena">HARD</BannerBtn>
+                </Button_corner>
+            </Bgimage>
 
 
 
