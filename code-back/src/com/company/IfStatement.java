073a284d295fae0fc7703e65e1188d8ff28b798e
Qaizx <kaothom@gmail.com>
2022-02-20 17:16:02 +0700

    เปลี่ยนการ implement Statement -> Expressions

--- code-back/src/com/company/IfStatement.java
+++ code-back/src/com/company/IfStatement.java
@@ -1,22 +1,25 @@
 package com.company;
 
-public class IfStatement implements Statement{
-    private Expressions check;
-    private Statement True;
-    private Statement False;
+import java.util.Map;
 
-    public IfStatement(Expressions ch, Statement t){
+public class IfStatement implements Expressions{
+    private Expressions check;
+    private Expressions True;
+    private Expressions False;
+
+    public IfStatement(Expressions ch, Expressions t){
         check = ch;
         True = t;
     }
 
-    public IfStatement(Expressions ch, Statement t, Statement f){
+    public IfStatement(Expressions ch, Expressions t, Expressions f){
         this(ch,t);
         False = f;
     }
 
-    @Override
-    public void eval() {
 
+    @Override
+    public int eval(Map<String, Integer> binding) {
+        return 0;
     }
 }
