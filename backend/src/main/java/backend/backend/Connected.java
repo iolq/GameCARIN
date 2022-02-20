ee7ab4be304596deb4925a09c9dbd293bf26db09
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2022-02-20 21:54:07 +0700

    Start for spring

---
+++ backend/src/main/java/backend/backend/Connected.java
@@ -0,0 +1,16 @@
+package backend.backend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/Game")
+public class Connected {
+    
+    @CrossOrigin
+    @RequestMapping(value = "/Stage")
+    public void Stage(){}
+
+
+}
