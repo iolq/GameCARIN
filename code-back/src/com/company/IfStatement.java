bedae8673e0d60318f25fc2575b5ff804a8b3966
Qaizx <kaothom@gmail.com>
2022-02-20 22:57:30 +0700

    เปลี่ยน override

--- code-back/src/com/company/IfStatement.java
+++ code-back/src/com/company/IfStatement.java
@@ -3,8 +3,8 @@ package com.company;
 import java.util.Map;
 
 public class IfStatement implements Expressions{
-    private Expressions check;
-    private Expressions True;
+    private final Expressions check;
+    private final Expressions True;
     private Expressions False;
 
     public IfStatement(Expressions ch, Expressions t){
@@ -12,7 +12,7 @@ public class IfStatement implements Expressions{
         True = t;
     }
 
-    public IfStatement(Expressions ch, Expressions t, Expressions f){
+    public IfStatement(Expressions ch, IfStatement t, IfStatement f){
         this(ch,t);
         False = f;
     }
@@ -20,6 +20,16 @@ public class IfStatement implements Expressions{
 
     @Override
     public int eval(Map<String, Integer> binding) {
+        return 0;
+    }
+
+    @Override
+    public int number() {
+        if(check.number() != 0){
+            True.number();
+        }else{
+            False.number();
+        }
         return 0;
     }
 }
