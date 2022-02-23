817ec70c8d280c745b1f1e9a3c08d0e62c48ccda
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2022-02-23 21:43:05 +0700

    Add class Main Game

---
+++ backend/src/main/java/backend/backend/Game.java
@@ -0,0 +1,19 @@
+package backend.backend;
+
+public class Game {
+    private Integer WidthScreen;
+    private Integer HeightScreen;
+    private Time time;
+    private int state;
+    private Inventory inventory;
+
+    public void GameLoop(){
+        while(true){
+            
+        }
+    }
+
+    public void Update(){
+            
+    }
+}
