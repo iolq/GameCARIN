2ab9e0af7627772976ccd0a51f16c6750cf60074
Qaizx <kaothom@gmail.com>
2022-02-24 18:06:57 +0700

    add class Unit implement Cell

---
+++ code-back/src/com/company/Unit.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Unit implements Cell{
+    @Override
+    public int moveTo() {
+        return 0;
+    }
+
+    @Override
+    public int shootTo() {
+        return 0;
+    }
+
+    @Override
+    public int Armor() {
+        return 0;
+    }
+
+    @Override
+    public int Hp() {
+        return 0;
+    }
+
+    @Override
+    public int DeHp() {
+        return 0;
+    }
+
+    @Override
+    public int DeArmor() {
+        return 0;
+    }
+}
