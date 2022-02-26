d36d32ab088f285ca56aec09a2aaa1a49b6ef6b4
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2022-02-26 16:59:44 +0700

    add method

---
+++ frontend/src/component/GameController.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+function GameController(){
+
+    var GetController = () =>{
+        axios.get("http://localhost:8080/game/State")
+        .then()
+    }
+
+    var PostController = () =>{
+        axios.post()
+        .then()
+    }
+    return(
+        <div></div>
+    );
+}
+export default GameController;
