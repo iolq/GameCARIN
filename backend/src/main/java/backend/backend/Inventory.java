1c4d551d23290994bde7df46fe7b58c9816c6a85
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2022-03-06 12:07:12 +0700

    add methode

--- backend/src/main/java/backend/backend/Inventory.java
+++ backend/src/main/java/backend/backend/Inventory.java
@@ -1,6 +1,13 @@
 package backend.backend;
 
+import java.util.List;
+
+import backend.com.company.Antibody;
+
 public class Inventory {
     //private Units Units;
     private int countUnits;
+    private List<Antibody> listAntibody;
+
+    public Inventory(){}
 }
