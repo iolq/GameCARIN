59314d10e42c5d8afa22ca696a54a31e176dddeb
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2022-03-06 12:06:58 +0700

    add methode

--- backend/src/main/java/backend/backend/Game.java
+++ backend/src/main/java/backend/backend/Game.java
@@ -11,13 +11,13 @@ public class Game {
     private Integer WidthScreen;
     private Integer HeightScreen;
     private Time time;
-    private int state;
+    private Integer stage;
     private Inventory inventory;
     private Area arena;
 
-    Game(Integer width,Integer height){
-        this.HeightScreen = height;
-        this.WidthScreen = width;
+    Game(){
+        this.time = new Time();
+        this.inventory = new Inventory();
         this.arena = new Area(WidthScreen, HeightScreen);
     }
 
@@ -25,34 +25,32 @@ public class Game {
         init();
         while(true){
             Update();
-            try{
-                Thread.sleep(3000);
-            }catch(InterruptedException e){}
-            arena.spawnVirus();
-            System.out.println(arena.countVirus);
-
+            
         }
     }
 
     public void Update(){
 
-            System.out.println(this.arena.countVirus);
 
     }
 
     //ตั้งค่าเริ่มต้น
     public void init(){
-        Antibody redCell = new Antibody();
-        Antibody whiteCell = new Antibody();
-        Antibody wallCell = new Antibody();
-        Virus coVid = new Virus();
-        Virus LFMO = new Virus();
-        Virus LMOFF = new Virus();
+        
     }
     public static void main(String[] args){
         
-        Game start = new Game(1320, 960);
+        Game start = new Game();
         start.GameLoop();
 
+    }
+
+    public void setGameStage(int number){
+        this.stage = number;
+    }
+
+    public int getGameStage(){
+
+        return this.stage;
     }
 }
