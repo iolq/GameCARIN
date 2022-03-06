4a9ddba26b1a7d3124e06043ffcdd1943e920a05
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2022-03-06 12:08:23 +0700

    this class is change puse and speed up

--- backend/src/main/java/backend/backend/Time.java
+++ backend/src/main/java/backend/backend/Time.java
@@ -2,4 +2,13 @@ package backend.backend;
 
 public class Time {
     private Integer speed;
+
+    public Time(){}
+    public Integer getTime(){
+        return this.speed;
+    }
+
+    public void setTime(Integer speed){
+        this.speed = speed;
+    }
 }
