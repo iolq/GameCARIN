955429d9d2809efd122fffd50294f74a1ed91c4b
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2022-03-09 14:54:18 +0700

    add infomation

--- backend/src/main/java/backend/backend/Time.java
+++ backend/src/main/java/backend/backend/Time.java
@@ -3,7 +3,10 @@ package backend.backend;
 public class Time {
     private Integer speed;
 
-    public Time(){}
+    public Time(){
+        // 1 = play , 2 = speed*2 , 0 = stop
+        this.speed = 1;
+    }
     public Integer getTime(){
         return this.speed;
     }
