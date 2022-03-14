13941078821102c5ed323975ec556aa3c8744974
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2022-03-14 21:55:19 +0700

    เพิ่มนิดนึง

--- frontend/src/component/arena.js
+++ frontend/src/component/arena.js
@@ -1,29 +1,55 @@
-import l2FVector from './2FVector';
+
 import React from 'react';
-import GameController from './GameController';
+
 import Config from './Config';
+import ImgBackground from './ImageBackground';
+
 
 var BackgroundArea1 = Config.Background;
 
 class Arena extends React.Component {
 
+    componentWillUnmount(){
+
+    }
+
+
     componentDidMount() {
 
+        this.init();
+        this.fetchUp();
+        this.updateUp();
         
     }
 
-    componentDidUpdate() { }
+    componentDidUpdate() {
+
+     }
 
 
     fetchUp() {
 
      }
 
+    updateUp(){
 
+    }
+
+    init(){
+
+        const canvas = document.querySelector('canvas')
+        canvas.width = 1920;
+        canvas.height = 1080;
+        var imbackground = new ImgBackground(BackgroundArea1,0,0,1920,1080)
+        imbackground.draw()
+    }
 
     render() {
         return (
-            <canvas />
+            <canvas 
+                width={800}
+                height={600}
+            />
         );
     }
 
