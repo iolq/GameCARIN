3c01b7d97f1146fb3afcb62a6b840446d3e373a9
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2022-03-14 21:55:34 +0700

    สำหรับวาดรูป

--- frontend/src/component/ImageBackground.js
+++ frontend/src/component/ImageBackground.js
@@ -4,15 +4,31 @@ class ImgBackground{
     canvas;
     context;
     Image;
+    ImgPath;
     positionX;
     positionY;
-
-    constructor(){
+    width;
+    height;
+    constructor(path,x,y,wid,hei){
+        this.canvas = document.querySelector('canvas');
+        this.context = this.canvas.getContext('2d')
+        this.Image=new Image();
+        this.ImgPath = path;
+        this.positionX=x;
+        this.positionY=y;
+        this.width=wid;
+        this.height=hei;
 
     }
 
     draw(){
-        
+        this.Image.src = this.ImgPath;
+        this.Image.onload=()=>console.log(this.Image)
+        this.context.drawImage(this.Image,this.positionX,this.positionY,this.width,this.height)
     }
 
+    
+
 }
+
+export default ImgBackground;
